809e625a3896dd9f6915f2b99a1f24bd4f7bb8d2
Ketki S Panse <ketki.skpanse@gmail.com>
2019-03-25 16:48:44 +0530

    added media queries for smaller screens

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ window.onscroll = () => {
   if(this.scrollY <= 450) nav.className = ''; else nav.className = 'scroll';
 };
 
+function menuopen() {
+  var checkBox = document.getElementById("toggle");
+  var menutab = document.getElementById("menu");
+
+  if (checkBox.checked == true){
+   menutab.style.display = "block";
+  } 
+  else {
+  	menutab.style.display = "none";
+  }
+}
